31891345795aad96d603ceb64c86ddbcb518aa3b
franssihombing <franssihombing09@gmail.com>
2024-03-04 14:44:23 +0700

    Your message about the commit

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,9 @@
+<nav>
+        <img class="logolitmas" src="{{ asset('assets/img/logo.png') }}" alt="">    
+        <ul>
+            <li> <a href="page.html"> Beranda </a> </li>
+            <li> <a href="page.html"> Layanan </a> </li>
+            <li> Kontak </li>
+            <li> Search ... </li>
+        </ul>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    @include('includes.head')
+
+<body>
+    
+    @include('includes.nav')
+    @yield('content')
+  
+</body>
+
+</html>
+
